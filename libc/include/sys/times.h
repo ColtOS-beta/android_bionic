spsforks/android-bionic-libc

Nullability check for time module.
Bugs: b/245972273
Test: adb shell
Change-Id: I61f69f48f3428ef1f544dd01e573ff078a9eab17

libc/include/sys/times.h
@@ -46,6 +46,6 @@ __BEGIN_DECLS
  * Returns a (possibly overflowed) absolute time on success,
  * and returns -1 and sets `errno` on failure.
  */
-clock_t times(struct tms* __buf);
+clock_t times(struct tms* __BIONIC_COMPLICATED_NULLNESS __buf);
 
 __END_DECLS

ColtOS-beta/android_bionic

Bug: b/245972273

libc/include/sys/times.h
@@ -46,6 +46,6 @@ __BEGIN_DECLS
  * Returns a (possibly overflowed) absolute time on success,
  * and returns -1 and sets `errno` on failure.
  */
-clock_t times(struct tms* __buf);
+clock_t times(struct tms* _Nullable __buf);
 
 __END_DECLS